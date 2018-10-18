e4b39a76a638594ded3d5be81accb99ef74d5cc4
Dmitry_Alexeev <dmitryironsity@gmail.com>
2018-10-18 23:55:01 +0700

    header is awesome

---
+++ docs/js/navigation.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+	var navToggleButton = $('.navigation__toggle');
+	var navToggleIcon = $('.navigation__toggle .fa');
+	var navBlock = $('.navigation__list');
+	var navBlockOpen = 'navigation__list--open';
+	var navLink = $('.navigation__list a');
+	var iconNav = 'fa-bars';
+	var iconClose = 'fa-times';
+
+	// Мобильная навигация
+	navToggleButton.on('click', function(e){
+		e.preventDefault();
+		navBlock.toggleClass(navBlockOpen);
+
+		if ( navToggleIcon.hasClass(iconNav) ){
+			navToggleIcon.removeClass(iconNav);
+			navToggleIcon.addClass(iconClose);
+		} else {
+			navToggleIcon.removeClass(iconClose);
+			navToggleIcon.addClass(iconNav);
+		}
+		
+	})
+
+	navLink.on('click', function(){
+
+		if ( navBlock.hasClass(navBlockOpen) ) {
+			if ( navToggleIcon.hasClass(iconNav) ){
+				navToggleIcon.removeClass(iconNav);
+				navToggleIcon.addClass(iconClose);
+			} else {
+				navToggleIcon.removeClass(iconClose);
+				navToggleIcon.addClass(iconNav);
+			}
+		}
+
+		navBlock.removeClass(navBlockOpen);
+		
+	})
+
+
+}); 
+
+//мобильная навигация Первоначальный вид
+	// $('.navigation__toggle').on('click', function(e){
+	// 	e.preventDefault();
+	// 	$('.navigation__list').toggleClass('navigation__list--open');
+
+	// 	if ( $('.navigation__toggle .fa').hasClass('fa-bars') ){
+			
+	// 		$('.navigation__toggle .fa').removeClass('fa-bars');
+	// 		$('.navigation__toggle .fa').addClass('fa-times');
+
+	// 	} else {
+
+	// 		$('.navigation__toggle .fa').removeClass('fa-times');
+	// 		$('.navigation__toggle .fa').addClass('fa-bars');
+
+	// 	}
+
+
+	// })

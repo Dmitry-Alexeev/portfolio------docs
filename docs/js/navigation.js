edbbb7a030eb3236896087908b30e30031b80b4a
Dmitry_Alexeev <dmitryironsity@gmail.com>
2018-10-23 21:20:57 +0700

    готовы валидация и иконка, php не проверен

--- docs/js/navigation.js
+++ docs/js/navigation.js
@@ -1,63 +1,68 @@
 $(document).ready(function() {
 
-	var navToggleButton = $('.navigation__toggle');
+  	// Закомментировано после переноса if-else в строчки 35-40 
+	// var toggle = $("#navigation__button");
+	// toggle.on("click", function(e){
+	// 	e.preventDefault();
+	//  if ( toggle.hasClass("active")) {
+	//    toggle.removeClass("active");
+	//  } else {
+	//    toggle.addClass("active");
+	//  }
+	// })
+
+	var navToggleButton = $('#navigation__button');
 	var navToggleIcon = $('.navigation__toggle .fa');
 	var navBlock = $('.navigation__list');
 	var navBlockOpen = 'navigation__list--open';
 	var navLink = $('.navigation__list a');
-	var iconNav = 'fa-bars';
-	var iconClose = 'fa-times';
+	// var iconNav = 'fa-bars';     //эти иконки мы не используем, удаляем код
+	// var iconClose = 'fa-times';
 
-	// Мобильная навигация
+	// Мобильная навигация (отвечает за открытие меню и смена иконки по клику)
 	navToggleButton.on('click', function(e){
 		e.preventDefault();
 		navBlock.toggleClass(navBlockOpen);
 
-		if ( navToggleIcon.hasClass(iconNav) ){
-			navToggleIcon.removeClass(iconNav);
-			navToggleIcon.addClass(iconClose);
-		} else {
-			navToggleIcon.removeClass(iconClose);
-			navToggleIcon.addClass(iconNav);
-		}
+		if ( navToggleButton.hasClass("active")) {
+			   navToggleButton.removeClass("active");
+			 } else {
+			   navToggleButton.addClass("active");
+			 }
+
+		// if ( navToggleIcon.hasClass(iconNav) ){
+		// 	navToggleIcon.removeClass(iconNav);
+		// 	navToggleIcon.addClass(iconClose);
+		// } else {
+		// 	navToggleIcon.removeClass(iconClose);
+		// 	navToggleIcon.addClass(iconNav);
+		// }
 		
 	})
 
+	// (отвечает за клин по ссылке в меню, чтобы меню ЗАКРЫЛОСЬ, а иконка стала ГАМБУРГЕРОМ)
 	navLink.on('click', function(){
 
 		if ( navBlock.hasClass(navBlockOpen) ) {
-			if ( navToggleIcon.hasClass(iconNav) ){
-				navToggleIcon.removeClass(iconNav);
-				navToggleIcon.addClass(iconClose);
-			} else {
-				navToggleIcon.removeClass(iconClose);
-				navToggleIcon.addClass(iconNav);
-			}
+
+			if ( navToggleButton.hasClass("active")) {
+			   navToggleButton.removeClass("active");
+			 } else {
+			   navToggleButton.addClass("active");
+			 }
+
+			// if ( navToggleIcon.hasClass(iconNav) ){
+			// 	navToggleIcon.removeClass(iconNav);
+			// 	navToggleIcon.addClass(iconClose);
+			// } else {
+			// 	navToggleIcon.removeClass(iconClose);
+			// 	navToggleIcon.addClass(iconNav);
+			// }
 		}
 
-		navBlock.removeClass(navBlockOpen);
-		
+		navBlock.removeClass(navBlockOpen);		
 	})
 
 
 }); 
 
-//мобильная навигация Первоначальный вид
-	// $('.navigation__toggle').on('click', function(e){
-	// 	e.preventDefault();
-	// 	$('.navigation__list').toggleClass('navigation__list--open');
-
-	// 	if ( $('.navigation__toggle .fa').hasClass('fa-bars') ){
-			
-	// 		$('.navigation__toggle .fa').removeClass('fa-bars');
-	// 		$('.navigation__toggle .fa').addClass('fa-times');
-
-	// 	} else {
-
-	// 		$('.navigation__toggle .fa').removeClass('fa-times');
-	// 		$('.navigation__toggle .fa').addClass('fa-bars');
-
-	// 	}
-
-
-	// })
